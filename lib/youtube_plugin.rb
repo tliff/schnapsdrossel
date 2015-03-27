32c67d062d7d8901b5887584041f8233a6b2955a
Stefan Maier <stefanmaier@gmail.com>
2015-03-27 14:59:43 +0100

    Output length of youtube videos

--- lib/youtube_plugin.rb
+++ lib/youtube_plugin.rb
@@ -8,8 +8,18 @@ module Schnapsdrossel
     match %r!(?:\A|\s)(https?://\S*youtu[\.]?be\S+)(?:\z|\s)!i, use_prefix: false
     
     def execute(m, url)
-      title = Nokogiri::HTML(open(url)).title.gsub(/ - YouTube$/, '')
-      m.channel.msg("YouTube: #{title}")
+      data = Nokogiri::HTML(open(url))
+      title = data.title.gsub(/ - YouTube$/, '')
+      duration = nil
+      data.css('[itemprop="duration"]').first['content'].gsub(/PT(\d+)M(\d+)S/) do |_|
+        minutes, seconds = $1.to_i, $2.to_i
+        if minutes >= 60
+          duration = "%.2i:%.2i:%.2i" % (minutes / 60, minutes % 60, seconds)
+        else
+          duration = "%.2i:%.2i" % (minutes, seconds)
+        end
+      end
+      m.channel.msg("YouTube: #{title} (#{duration})")
     end
 
   end
