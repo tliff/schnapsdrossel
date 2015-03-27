b136836862293c625ea9a09c00c18c16c0bb8eaa
Stefan Maier <stefanmaier@gmail.com>
2015-03-27 15:01:27 +0100

    fix typo

--- lib/youtube_plugin.rb
+++ lib/youtube_plugin.rb
@@ -14,9 +14,9 @@ module Schnapsdrossel
       data.css('[itemprop="duration"]').first['content'].gsub(/PT(\d+)M(\d+)S/) do |_|
         minutes, seconds = $1.to_i, $2.to_i
         if minutes >= 60
-          duration = "%.2i:%.2i:%.2i" % (minutes / 60, minutes % 60, seconds)
+          duration = "%.2i:%.2i:%.2i" % [minutes / 60, minutes % 60, seconds]
         else
-          duration = "%.2i:%.2i" % (minutes, seconds)
+          duration = "%.2i:%.2i" % [minutes, seconds]
         end
       end
       m.channel.msg("YouTube: #{title} (#{duration})")
