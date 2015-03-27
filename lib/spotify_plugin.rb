1883c08bbf0b7ab9134ebc6feda1324c9cac0f49
Stefan Maier <stefanmaier@gmail.com>
2015-03-27 13:59:39 +0100

    Remove broken output in spotify

--- lib/spotify_plugin.rb
+++ lib/spotify_plugin.rb
@@ -7,7 +7,6 @@ module Schnapsdrossel
     match /spotify(.com?)[:\/](track|album)[:\/](.*)/, use_prefix: false
   
     def execute(m, _, type, identifier)
-      puts config[:tumblr]
       xml = Nokogiri::XML(open("http://ws.spotify.com/lookup/1/?uri=spotify:#{type}:#{identifier}").read)
       xml.remove_namespaces!
       track_name = xml.at_xpath("/#{type}/name").content rescue ''
