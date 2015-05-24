6a49b552899098722a2a384d7d454fe5398ff0cb
Stefan Maier <stefanmaier@gmail.com>
2015-05-24 22:04:10 +0200

    Run the Twitter stream in a seperate thread

--- lib/twitter_plugin.rb
+++ lib/twitter_plugin.rb
@@ -11,35 +11,36 @@ module Schnapsdrossel
     #match /\!(\w+)\s*(.*)/, use_prefix: false
 
     def execute
-      
+
     end
-    
+
     def setup_listener(_)
       puts "On connect"
       done_tweets = []
-      pp config
-      loop do
-        begin
-          client = Twitter::Streaming::Client.new do |conf|
-            config[:twitter].each do |k,v|
-              conf.send("#{k}=".to_sym, v)
+      Thread.new do
+        loop do
+          begin
+            client = Twitter::Streaming::Client.new do |conf|
+              config[:twitter].each do |k,v|
+                conf.send("#{k}=".to_sym, v)
+              end
             end
-          end
-          client.user do |message|
-            if message.is_a? Twitter::Tweet
-              if !done_tweets.member?(message.id)
-                done_tweets << message.id
-                puts "Tweet by #{message.user.name}: #{message.text}"
-                Channel(config[:channel]).msg "Tweet by @#{message.user.screen_name} (#{message.user.name}): #{HTMLEntities.new.decode message.text}".strip.gsub(/\n+/, ' | ') if message.retweeted_status.is_a?(Twitter::NullObject)
-                if !message.retweeted_status.is_a?(Twitter::NullObject) && !done_tweets.member?(message.retweeted_status.id)
-                  done_tweets << message.retweeted_status.id
-                  Channel(config[:channel]).msg "Tweet by @#{message.user.screen_name} (#{message.user.name}): RT #{HTMLEntities.new.decode message.retweeted_status.user.screen_name} #{message.retweeted_status.text}".strip.gsub(/\n+/, ' | ')
+            client.user do |message|
+              if message.is_a? Twitter::Tweet
+                if !done_tweets.member?(message.id)
+                  done_tweets << message.id
+                  puts "Tweet by #{message.user.name}: #{message.text}"
+                  Channel(config[:channel]).msg "Tweet by @#{message.user.screen_name} (#{message.user.name}): #{HTMLEntities.new.decode message.text}".strip.gsub(/\n+/, ' | ') if message.retweeted_status.is_a?(Twitter::NullObject)
+                  if !message.retweeted_status.is_a?(Twitter::NullObject) && !done_tweets.member?(message.retweeted_status.id)
+                    done_tweets << message.retweeted_status.id
+                    Channel(config[:channel]).msg "Tweet by @#{message.user.screen_name} (#{message.user.name}): RT #{HTMLEntities.new.decode message.retweeted_status.user.screen_name} #{message.retweeted_status.text}".strip.gsub(/\n+/, ' | ')
+                  end
                 end
               end
             end
+            #rescue
+            #next
           end
-          #rescue
-          #next
         end
       end
     end
