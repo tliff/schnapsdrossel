f632b7ca7255ff4dd745609d960649bbaeee19aa
Nico Rieck <nico.rieck@gmail.com>
2015-12-21 01:10:19 +0100

    Fix access check in definitions plugin

--- lib/definitions_plugin.rb
+++ lib/definitions_plugin.rb
@@ -8,6 +8,7 @@ module Schnapsdrossel
 
     def initialize(*args)
       super
+      @access_checker = config[:access_checker] || lambda { false }
       @definitions = {}
       if File.exist?(DEFINITION_FILE)
         @definitions = YAML.load(File.open(DEFINITION_FILE))
@@ -16,17 +17,18 @@ module Schnapsdrossel
 
     match /define\s+(\w+)\s+(.*)/, prefix: '.', method: :define
     match /\!(\w+)\s*(.*)/, use_prefix: false
-    
-     def execute(m, verb, arguments)
+
+    def execute(m, verb, arguments)
       if definition = @definitions[verb]
         m.channel.msg("#{definition} #{arguments.lstrip}")
       end
     end 
 
     def define(m, verb, action)
-      if MASTERS.member?(m.user.host)
+      if @access_checker.call(m.user)
         @definitions[verb] = action
         File.write(DEFINITION_FILE, @definitions.to_yaml)
+        m.channel.msg("#{verb} updated")
       end
     end
 
