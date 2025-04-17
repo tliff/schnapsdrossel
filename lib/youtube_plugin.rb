d3b9bcd4e6cdd59b32218a9d292fc8884741e2d9
Stefan Maier <stefan.maier@mga.de>
2025-04-17 10:58:44 +0200

    Revert "Add channel description"
    This reverts commit 13547ef93746c09007f1351182e784b02fb80bfd.

--- lib/youtube_plugin.rb
+++ lib/youtube_plugin.rb
@@ -6,42 +6,10 @@ module Schnapsdrossel
     include Cinch::Plugin
     REGEX =  %r!(?<=\A|\s)(https?://\S*youtu[\.]?be\S+)(?=\z|\s)!i
     match REGEX, use_prefix: false
-    
-    DESCRIPTION_LENGTH = 100
 
     def execute(m, url)
       urls = m.message.scan(REGEX)
-      m.channel.msg("Youtube: #{urls.map{|url| process_url(url.first)}.join(' | ')}")
-    end
-
-    def process_url(url)
-      uri = URI(url)
-      
-      # Channel URL detection (format: youtube.com/@channelname or youtube.com/c/channelname)
-      if uri.path.start_with?('/@') || uri.path.start_with?('/c/')
-        return channel_data(url)
-      end
-      
-      # Regular video processing
-      video_data(url)
-    end
-
-    def channel_data(url)
-      uri = URI(url)
-      channel_handle = uri.path.gsub(/\A\/(@|c\/)/, '')
-      
-      begin
-        channel = Yt::Channel.new(id: channel_handle)
-        title = channel.title
-        description = channel.description.to_s
-        truncated_description = description.length > DESCRIPTION_LENGTH ? 
-                               "#{description[0...DESCRIPTION_LENGTH]}..." : 
-                               description
-        
-        "Channel: #{title} - #{truncated_description}"
-      rescue => e
-        "Invalid YouTube channel: #{e.message}"
-      end
+      m.channel.msg("Youtube: #{urls.map{|url| video_data(url.first)}.join(' | ')}")
     end
 
     def video_data(url)
@@ -69,5 +37,7 @@ module Schnapsdrossel
       end
       "#{title} (#{duration})"
     end
+
   end
+
 end
