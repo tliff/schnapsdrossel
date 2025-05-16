28454ace68e5b23f89864e13d1b7d28b5acb9245
Christoph Pegel <6085285+cpegel@users.noreply.github.com>
2025-05-16 11:11:09 +0200

    Use nitter.tiekoetter.com

--- lib/twitter_plugin.rb
+++ lib/twitter_plugin.rb
@@ -4,12 +4,13 @@ require 'nokogiri'
 module Schnapsdrossel
   class TwitterPlugin
     include Cinch::Plugin
+    NITTER_HOST = 'nitter.tiekoetter.com'
     REGEX =  %r!(?<=\A|\s)(https?://(?:www\.)?(?:x|twitter).com/\S+)(?=\z|\s)!i
     match REGEX, use_prefix: false
 
     def execute(m, url)
       urls = m.message.scan(REGEX).flatten
-      m.channel.msg("Nitter: #{urls.map{|url| url.gsub(%r!(?:www\.)?(?:x|twitter).com/!, 'twiiit.com/')}.join(' | ')}")
+      m.channel.msg("Nitter: #{urls.map{|url| url.gsub(%r!(?:www\.)?(?:x|twitter).com/!, "#{NITTER_HOST}/")}.join(' | ')}")
     end
 
   end
